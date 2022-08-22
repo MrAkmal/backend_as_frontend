00b589ffa8a23c34a1a28830601e313ce16644c8
Asus <mr.akmal4555@mail.ru>
2022-08-22 14:08:58 +0500

    Initial commit

---
+++ src/main/java/com/example/backend_as_frontend/BackendAsFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_as_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAsFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAsFrontendApplication.class, args);
+    }
+
+}
