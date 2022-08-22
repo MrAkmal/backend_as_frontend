e734762d911c89495fa738c0163f60192f1d259c
Asus <mr.akmal4555@mail.ru>
2022-08-22 15:27:36 +0500

    reactive

--- src/main/java/com/example/backend_as_frontend/service/ProcurementMethodService.java
+++ src/main/java/com/example/backend_as_frontend/service/ProcurementMethodService.java
@@ -1,11 +1,17 @@
 package com.example.backend_as_frontend.service;
 
+import com.example.backend_as_frontend.entity.ProcurementMethod;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 public class ProcurementMethodService {
-
 
 
 
@@ -18,30 +24,73 @@ public class ProcurementMethodService {
         this.webClient = webClient;
     }
 
-    public void  get(){
+    public ProcurementMethod get(Integer id) {
 
-        webClient.get()
-                .uri(baseURI);
-    }
+        Mono<ProcurementMethod> entity = webClient.get()
+                .uri(baseURI + "/" + id)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToMono(ProcurementMethod.class);
 
-
-    public void  getAll(){
-
-    }
-
-
-    public void  save(){
+        return entity.block();
 
     }
 
 
-    public void  update(){
+    public List<ProcurementMethod> getAll() {
+
+        Flux<ProcurementMethod> entity = webClient.get()
+                .uri(baseURI)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(ProcurementMethod.class);
+
+        return entity.collectList().block();
+    }
+
+
+    public void save(ProcurementMethod nature) {
+
+
+        Mono<ProcurementMethod> entity = webClient.post()
+                .uri(baseURI)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(Mono.just(nature), ProcurementMethod.class)
+                .retrieve()
+                .bodyToMono(ProcurementMethod.class);
+
+        System.out.println("entity.block() = " + entity.block());
+
 
     }
 
 
-    public void  delete(){
+    public void update(ProcurementMethod nature) {
+
+
+        Mono<ProcurementMethod> entity = webClient.put()
+                .uri(baseURI)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(Mono.just(nature), ProcurementMethod.class)
+                .retrieve()
+                .bodyToMono(ProcurementMethod.class);
+
+        System.out.println("entity.block() = " + entity.block());
+
 
     }
 
+
+    public void delete(Integer id) {
+
+        Mono<ProcurementMethod> entity = webClient.delete()
+                .uri(baseURI + "/" + id)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToMono(ProcurementMethod.class);
+
+
+        System.out.println("entity.block() = " + entity.block());
+
+    }
 }
