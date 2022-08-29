531df74ddd6fbf2fbaf25b0cf57febf2293f578a
asadbekEngineere <asadbek.software.engineere@gmail.com>
2022-08-29 18:34:27 +0500

    payment-config

---
+++ src/main/java/com/example/backend_as_frontend/controller/PaymentConfigController.java
@@ -0,0 +1,45 @@
+package com.example.backend_as_frontend.controller;
+
+import com.example.backend_as_frontend.entity.ProcurementMethod;
+import com.example.backend_as_frontend.entity.ProcurementNature;
+import com.example.backend_as_frontend.service.ProcurementMethodService;
+import com.example.backend_as_frontend.service.ProcurementNatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/payment-config")
+public class PaymentConfigController {
+
+    private final ProcurementNatureService procurementNatureService;
+    private final ProcurementMethodService procurementMethodService;
+
+
+    @Autowired
+    public PaymentConfigController(ProcurementNatureService procurementNatureService, ProcurementMethodService procurementMethodService) {
+        this.procurementNatureService = procurementNatureService;
+        this.procurementMethodService = procurementMethodService;
+    }
+
+    @GetMapping
+    public String getList() {
+
+        return "paymentconfig/payment-config";
+    }
+
+    @GetMapping("/create")
+    public String createForm(Model model) {
+
+        List<ProcurementMethod> all = procurementMethodService.getAll();
+        List<ProcurementNature> procurementNatureServiceAll = procurementNatureService.getAll();
+        model.addAttribute("procurementMethods", all);
+        model.addAttribute("types", List.of("DOCUMENT", "FEE", "REGISTER", "PE"));
+        model.addAttribute("procurementNatures", procurementNatureServiceAll);
+        return "paymentconfig/payment-config-form";
+    }
+}
